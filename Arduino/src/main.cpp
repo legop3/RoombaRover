f098d3e505c8092b029448d09dea69a74a17b289
legop3 <minecratfd8@gmail.com>
2025-04-16 01:16:25 -0400

    arduino pulse script done

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,45 +1,15 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
-#define SERIAL_RX_PIN 10
-#define SERIAL_TX_PIN 11
-#define PULSE_PIN 8
-#define PULSE_INTERVAL 270000UL // 4.5 minutes in ms
-#define PULSE_DURATION 1000UL   // 1 second in ms
-
-SoftwareSerial serialPins(SERIAL_RX_PIN, SERIAL_TX_PIN);
-
-unsigned long lastPulse = 0;
-bool pulsing = false;
-unsigned long pulseStart = 0;
+const int pin = 8;
 
 void setup() {
-    Serial.begin(115200);
-    serialPins.begin(115200);
-    pinMode(PULSE_PIN, OUTPUT);
-    digitalWrite(PULSE_PIN, HIGH); // Default HIGH
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH); // Start with pin HIGH
 }
 
 void loop() {
-    // Forward USB Serial to serial pins
-    while (Serial.available()) {
-        serialPins.write(Serial.read());
-    }
-    // Forward serial pins to USB Serial (optional)
-    while (serialPins.available()) {
-        Serial.write(serialPins.read());
-    }
-
-    unsigned long now = millis();
-
-    if (!pulsing && now - lastPulse >= PULSE_INTERVAL) {
-        digitalWrite(PULSE_PIN, LOW);
-        pulsing = true;
-        pulseStart = now;
-    }
-    if (pulsing && now - pulseStart >= PULSE_DURATION) {
-        digitalWrite(PULSE_PIN, HIGH);
-        pulsing = false;
-        lastPulse = now;
-    }
+  digitalWrite(pin, LOW);   // Pull pin 8 LOW
+  delay(1000);              // Wait 1 second
+  digitalWrite(pin, HIGH);  // Set pin 8 HIGH
+  delay(59000);             // Wait remaining 59 seconds
 }
