62be7df14bdabb4384144bc92c5883cb33cd2e44
legop3 <minecratfd8@gmail.com>
2025-05-09 15:50:14 -0400

    epiphany application mode

--- server.js
+++ server.js
@@ -609,7 +609,7 @@ server.listen(webport, () => {
         // });
 
         // for epiphany
-        exec(`epiphany -a http://localhost:${webport}/viewer`, (error, stdout, stderr) => {
+        exec(`epiphany --application-mode=http://localhost:${webport}/viewer`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error opening epiphany: ${error.message}`);
                 return;
