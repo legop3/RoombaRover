801511113978c36b6dd887ca251b70a8f0047bb9
legop3 <minecratfd8@gmail.com>
2025-07-09 22:39:14 -0400

    fix

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ port.on('data', (data) => {
 
 
 function isValidPacket(data) {
-    if (data.length !== EXPECTED_PACKET_SIZE) return false;
+    if (data.length !== expectedPacketLength) return false;
     
     try {
         // Basic validation checks - adjust these based on your robot's expected ranges
