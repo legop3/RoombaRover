c57dd92d086b58121645e2201988d6bd3155f29b
legop3 <46182676+legop3@users.noreply.github.com>
2025-09-04 22:44:29 -0400

    Stream camera via Socket.IO

--- CameraStream.js
+++ CameraStream.js
@@ -11,11 +11,8 @@ class CameraStream {
         this.height = options.height || 240;
         this.fps = options.fps || 15;
         this.quality = options.quality || 5;
-        this.interval = 1000 / this.fps;
         this.ffmpeg = null;
         this.streaming = false;
-        this.latestFrame = null;
-        this.sendFrameInterval = null;
     }
 
     start() {
@@ -33,34 +30,14 @@ class CameraStream {
             '-q:v', String(this.quality),
             '-preset', 'ultrafast',
             '-an',
-            '-f', 'image2pipe',
-            '-vcodec', 'mjpeg',
+            '-vcodec', 'mpeg1video',
+            '-f', 'mpegts',
             'pipe:1',
         ]);
 
-        let frameBuffer = Buffer.alloc(0);
-
-        this.ffmpeg.stdout.on('data', (chunk) => {
-            frameBuffer = Buffer.concat([frameBuffer, chunk]);
-            let start, end;
-            while ((start = frameBuffer.indexOf(Buffer.from([0xFF, 0xD8]))) !== -1 &&
-                   (end = frameBuffer.indexOf(Buffer.from([0xFF, 0xD9]), start)) !== -1) {
-                const frame = frameBuffer.slice(start, end + 2);
-                frameBuffer = frameBuffer.slice(end + 2);
-                this.latestFrame = frame;
-            }
+        this.ffmpeg.stdout.on('data', (data) => {
+            this.io.emit(`${this.cameraId}:data`, data);
         });
-
-        this.sendFrameInterval = setInterval(() => {
-            if (this.latestFrame) {
-                const frameToSend = this.latestFrame;
-                this.latestFrame = null;
-                this.io.emit(`videoFrame:${this.cameraId}`, frameToSend.toString('base64'));
-                if (this.cameraId === 'frontCamera') {
-                    latestFrontFrame = frameToSend.toString('base64');
-                }
-            }
-        }, this.interval);
 
         this.ffmpeg.stderr.on('data', (data) => {
             this.io.emit(`ffmpeg`, data.toString());
@@ -83,12 +60,6 @@ class CameraStream {
             this.ffmpeg = null;
         }
         this.streaming = false;
-        this.latestFrame = null;
-
-        if (this.sendFrameInterval) {
-            clearInterval(this.sendFrameInterval);
-            this.sendFrameInterval = null;
-        }
     }
 }
 
