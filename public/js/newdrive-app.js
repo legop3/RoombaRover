c1af2d088112d6d524649ae32b2aa27d9c0fdc42
legop3 <minecratfd8@gmail.com>
2025-10-17 23:57:38 -0400

    add modules to newdrive js

--- public/js/newdrive-app.js
+++ public/js/newdrive-app.js
@@ -1,9 +1,25 @@
-import './modules/socketGlobal.js';
+import './modules/features.js';
+import './modules/uiState.js';
+import './modules/uiConfig.js';
+import './modules/hudAndSensors.js';
 import './modules/adminLogin.js';
 import './modules/homeAssistantLights.js';
+import './modules/iframeAutomation.js';
 import './modules/roomCamera.js';
-import './modules/presence.js';
 import './modules/adminControls.js';
+import './modules/mediaControls.js';
+import './modules/driverControls.js';
+import './modules/nicknames.js';
+import './modules/turns.js';
+import './modules/chatSend.js';
+import './modules/chatGet.js';
+import './modules/logs.js';
+import './modules/notifications.js';
+import './modules/presence.js';
+import './modules/ollama.js';
+import './modules/connection.js';
+import './modules/webcam.js';
+import './modules/maintenance.js';
 import { initializeDriveIframes } from './newdriveCore/driveIframeManager.js';
 import { initializeTabs } from './newdriveCore/tabs.js';
 
