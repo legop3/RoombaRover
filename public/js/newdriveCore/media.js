d49263a9631602aca194c2f03cd620ddbba3a459
legop3 <minecratfd8@gmail.com>
2025-10-18 20:01:37 -0400

    add fix for my weird phone lol

--- public/js/newdriveCore/media.js
+++ public/js/newdriveCore/media.js
@@ -6,6 +6,11 @@ const LayoutState = Object.freeze({
 
 const desktopQuery = window.matchMedia('(min-width: 1024px)');
 const landscapeQuery = window.matchMedia('(orientation: landscape)');
+const coarsePointerQuery = window.matchMedia('(pointer: coarse)');
+
+function isTouchDevice() {
+  return coarsePointerQuery.matches || (typeof navigator !== 'undefined' && navigator.maxTouchPoints > 1);
+}
 
 function addMediaListener(query, handler) {
   if (typeof query.addEventListener === 'function') {
@@ -22,6 +27,12 @@ function addMediaListener(query, handler) {
 }
 
 function getLayoutState() {
+  const touch = isTouchDevice();
+
+  if (touch) {
+    return landscapeQuery.matches ? LayoutState.MOBILE_LANDSCAPE : LayoutState.MOBILE_PORTRAIT;
+  }
+
   if (desktopQuery.matches) {
     return LayoutState.DESKTOP;
   }
@@ -42,12 +53,14 @@ function subscribeMedia(callback) {
   window.addEventListener('orientationchange', handler);
   const removeDesktop = addMediaListener(desktopQuery, handler);
   const removeLandscape = addMediaListener(landscapeQuery, handler);
+  const removePointer = addMediaListener(coarsePointerQuery, handler);
 
   return () => {
     window.removeEventListener('resize', handler);
     window.removeEventListener('orientationchange', handler);
     removeDesktop();
     removeLandscape();
+    removePointer();
   };
 }
 
